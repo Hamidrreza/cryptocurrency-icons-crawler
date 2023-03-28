af40c0c5e6027ba1826060296c1a16d4b77e014d
Hamidreza <hamid@hamid.com>
2023-03-28 23:08:39 +0430

    adding readme.md,,, configs part,,, gitignore file

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # cryptocurrency-icons-crawler
+## web crawler with python using BeautifulSoup library.
 this code is a python web crawler that download about 8000 icons of cryptocurrencies
---
+++ configs.py
@@ -0,0 +1,5 @@
+# config file for personal configrations of code.
+
+SITE_URL = 'https://arzdigital.com/coins/page-'
+FILE_ADDRESS = './icons/'
+ICONS_TAG_ADDR = 'arz-coin-table__name-td arz-sort-value arz-scroll-column arz-middle-off'
